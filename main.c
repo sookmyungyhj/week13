dc07c5711a6eb716b11c71656433814c01d2d24b
sookmyungyhj <70833975+sookmyungyhj@users.noreply.github.com>
2020-11-26 17:12:17 +0900

    3

--- main.c
+++ main.c
@@ -1,22 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "keyword.h"
 
+
+int is_whitespace(char c){
+	if( c == ' '||
+		c == '\n'||
+		c == '\r'||
+		c == '\t'||
+		c == '(')
+		return 1;
+		
+	return 0;
+}
+
+int fget_word(FILE *fp, char*word){
+
+	char c;
+	int cnt;
+	
+	while((c=fgetc(fp))!=EOF){
+		if(is_whitespace(c)==0)
+			break;
+	}
+	
+	if(c==EOF)
+		return 0;
+	//filing out the word
+	cnt = 0; 
+	word[cnt++]=c;
+	word[cnt]='\0';
+	
+	while((word[cnt]=fgetc(fp)) !=EOF){
+		if(is_whitespace(word[cnt])==1){
+			word[cnt]='\0';
+			break;
+		}
+		cnt++;
+	}
+	return cnt;
+}
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-struct student{
-	int id;
-	char name[10];
-	double grade;
-};
 
 void main(void) {
-	struct student A = {123456,"Juyeop",4.3};
+	FILE*fp; 
+	char filename[100];
+	char word[100];
 	
-	A.id = 654321;
-	A.grade = 4.100000;
+	int cnt;
+	//file open
 	
-	printf("ID : %d \n",A.id);
-	printf("NAME : %s \n",A.name);
-	printf("GRADE : %f \n",A.grade);
+	printf("Input the file name : ");
+	scanf("%s",filename);
+	
+	if ((fp = fopen(filename,"r")) == NULL){
+		printf("파일을 못 열음.(%s) \n",filename);
+		return -1;
+	}
+
+	//bring words from fp
+	while(fget_word(fp,word)!=0){ //bring a word
+		count_word(word);
+	}
+	print_word();
+	//bring a word
+	
+	fclose(fp);
 	
 	return 0;
 }
